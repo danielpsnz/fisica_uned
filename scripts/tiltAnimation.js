df2b5ee954f9345e537efed736f1d7ccd2c3004d
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-01-01 03:55:01 +0100

    added some html files and javascript features

---
+++ scripts/tiltAnimation.js
@@ -0,0 +1,8 @@
+import VanillaTilt from "vanilla-tilt";
+
+export default function initTiltAnimation() {
+  const elements = document.querySelectorAll(".js-tilt");
+  VanillaTilt.init(elements);
+}
+
+initTiltEffect();
